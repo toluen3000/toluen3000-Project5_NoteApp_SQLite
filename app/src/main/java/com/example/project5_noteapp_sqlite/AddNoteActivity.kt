e21a65b3d361329d8585c1b369dc112a93466e42
toluen3000 <toluen3000@gmail.com>
2024-06-25 10:14:24 +0700

    Create Database Successfully

--- app/src/main/java/com/example/project5_noteapp_sqlite/AddNoteActivity.kt
+++ app/src/main/java/com/example/project5_noteapp_sqlite/AddNoteActivity.kt
@@ -1,20 +1,35 @@
 package com.example.project5_noteapp_sqlite
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.project5_noteapp_sqlite.databinding.ActivityAddNoteBinding
+import com.example.project5_noteapp_sqlite.databinding.ActivityMainBinding
 
 class AddNoteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db:NoteDatabaseHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_add_note)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //crate db
+        db = NoteDatabaseHelper(this)
+
+        binding.saveButton.setOnClickListener {
+
+            val title = binding.edtNameNote.text.toString()
+            val content = binding.edtDescription.text.toString()
+            val note = Note(0,title,content)
+
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this,"Note Saved",Toast.LENGTH_SHORT).show()
         }
     }
 }
---
+++ app/src/main/java/com/example/project5_noteapp_sqlite/Note.kt
@@ -0,0 +1,6 @@
+package com.example.project5_noteapp_sqlite
+
+data class Note(val id:Int,
+                val title: String,
+                val content: String
+    )
